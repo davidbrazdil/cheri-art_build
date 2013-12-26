68280e7537b1841d1e6846bc70b8a750802c8100
David Brazdil <david.brazdil@gmail.com>
2013-12-26 10:59:09 +0000

    Set AndroidConfig.h for FreeBSD+MIPS target, config not present yet

--- core/combo/TARGET_freebsd-mips.mk
+++ core/combo/TARGET_freebsd-mips.mk
@@ -22,3 +22,4 @@ include $(BUILD_COMBOS)/$(combo_target)linux-mips.mk
 TARGET_TOOLCHAIN_ROOT := prebuilts/gcc/$(HOST_PREBUILT_TAG)/mips64/mips64-unknown-freebsd
 TARGET_TOOLS_PREFIX := $(TARGET_TOOLCHAIN_ROOT)/bin/mips64-unknown-freebsd-
 
+android_config_h := $(call select-android-config-h,freebsd-mips)
