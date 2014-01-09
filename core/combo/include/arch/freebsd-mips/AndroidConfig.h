7055a13add43376ffd4b68ffefc0c271c0071517
David Brazdil <david.brazdil@gmail.com>
2014-01-09 10:52:29 +0000

    FreeBSD does not have pthread_cond_timedwait_monotonic

--- core/combo/include/arch/freebsd-mips/AndroidConfig.h
+++ core/combo/include/arch/freebsd-mips/AndroidConfig.h
@@ -148,7 +148,7 @@
  * Define this if we have pthread_cond_timedwait_monotonic() and
  * clock_gettime(CLOCK_MONOTONIC).
  */
-#define HAVE_TIMEDWAIT_MONOTONIC
+/* #define HAVE_TIMEDWAIT_MONOTONIC */
 
 /*
  * Define this if we have linux style epoll()
