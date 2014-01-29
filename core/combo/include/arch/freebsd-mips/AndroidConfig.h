06c9afbb3134ab6a3b07198858937653d419f7ab
David Brazdil <david.brazdil@gmail.com>
2014-01-29 17:38:40 +0000

    Correct definition of off64_t on FreeBSD

--- core/combo/include/arch/freebsd-mips/AndroidConfig.h
+++ core/combo/include/arch/freebsd-mips/AndroidConfig.h
@@ -175,13 +175,13 @@
  * agree on the same size.  For desktop systems, use 64-bit values,
  * because some of our libraries (e.g. wxWidgets) expect to be built that way.
  */
-/* #define _FILE_OFFSET_BITS 64 */
-/* #define _LARGEFILE_SOURCE 1 */
+#define _FILE_OFFSET_BITS 64
+#define _LARGEFILE_SOURCE 1
 
 /*
  * Define if platform has off64_t (and lseek64 and other xxx64 functions)
  */
-#define HAVE_OFF64_T
+/* #define HAVE_OFF64_T */
 
 /*
  * Defined if we have the backtrace() call for retrieving a stack trace.
