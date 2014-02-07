2fc41a3bd87bdebf11ae2d974479982672e62457
David Brazdil <david.brazdil@gmail.com>
2014-02-07 21:46:16 +0000

    FreeBSD MIPS does not define LARGEFILE_SOURCE

--- core/combo/include/arch/freebsd-mips/AndroidConfig.h
+++ core/combo/include/arch/freebsd-mips/AndroidConfig.h
@@ -176,7 +176,7 @@
  * because some of our libraries (e.g. wxWidgets) expect to be built that way.
  */
 #define _FILE_OFFSET_BITS 64
-#define _LARGEFILE_SOURCE 1
+/* #define _LARGEFILE_SOURCE 1 */
 
 /*
  * Define if platform has off64_t (and lseek64 and other xxx64 functions)
