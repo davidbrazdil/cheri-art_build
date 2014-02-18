3dba3186b03aa3daded0ee173c4477a49a845d2e
David Brazdil <david.brazdil@gmail.com>
2014-02-18 22:42:00 +0000

    Fixed linker shared library problem

--- core/combo/TARGET_freebsd-mips.mk
+++ core/combo/TARGET_freebsd-mips.mk
@@ -39,7 +39,7 @@ define transform-o-to-shared-lib-inner
  $(hide) $(PRIVATE_CXX) \
      -Wl,-soname,$(notdir $@) \
      -Wl,--gc-sections \
-     -Wl,-shared,-Bsymbolic \
+     -shared \
      $(PRIVATE_TARGET_GLOBAL_LD_DIRS) \
      $(if $(filter true,$(PRIVATE_NO_CRT)),,$(PRIVATE_TARGET_CRTBEGIN_SO_O)) \
      $(PRIVATE_ALL_OBJECTS) \
